fbe365de9d67d8828ff295edade5c704d4072ba4
Kmalk3 <44727595+Kmalk3@users.noreply.github.com>
2018-12-11 16:19:25 +0100

    Add files via upload

---
+++ Spam1.js
@@ -0,0 +1,15 @@
+ const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By King_Power");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("510453176185389078")
+setInterval(function() {
+channel.send(`Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج [ " 3934 " ]
+`);
+}, 25)
+})
+ 
+ 
+client.login(process.env.TOKEN);
